f9766d2691a04fdbcf8af508759015a5593295ab
YoungsooJang <ylouang@kaist.ac.kr>
2017-01-02 00:01:04 +0900

    post facebook contacts to server

--- app/src/main/java/com/example/q/cs496_proj2/MainActivity.java
+++ app/src/main/java/com/example/q/cs496_proj2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.q.cs496_proj2;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
@@ -34,8 +35,15 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -209,6 +217,7 @@ public class MainActivity extends AppCompatActivity {
                                 data[1] = friendList.getJSONObject(i).getString("name");
                                 data[2] = "";
                                 facebookContacts.add(data);
+                                new PostFacebookContacts().execute(data);
                             }
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -216,5 +225,51 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         ).executeAsync();
+    }
+
+    public void sendHttpWithContact(String url, String json) {
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            //Log.d("RESPONSE", response.body().string());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String contactStringArrayToJSON(String[] stringArray) {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("name", stringArray[1]);
+            jsonObject.put("phoneNumber", stringArray[2]);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Log.d("JSON", jsonObject.toString());
+        return jsonObject.toString();
+    }
+
+    public class PostFacebookContacts extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(String... str) {
+            sendHttpWithContact("http://52.78.101.202:3000/api/contacts", contactStringArrayToJSON(str));
+            return null;
+        }
+
+        protected void onPostExecute(Void none){
+        }
     }
 }
