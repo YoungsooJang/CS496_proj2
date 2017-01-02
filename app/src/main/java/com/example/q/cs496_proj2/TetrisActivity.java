f57076aeb3c2c89cd990e03655e77dac383accf2
YoungsooJang <ylouang@kaist.ac.kr>
2017-01-03 04:54:11 +0900

    Tetris left, right, drop button and function

--- app/src/main/java/com/example/q/cs496_proj2/TetrisActivity.java
+++ app/src/main/java/com/example/q/cs496_proj2/TetrisActivity.java
@@ -1,12 +1,16 @@
 package com.example.q.cs496_proj2;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.GridView;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Random;
 
 public class TetrisActivity extends AppCompatActivity {
@@ -21,6 +25,9 @@ public class TetrisActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tetris);
 
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
         for (int i = 0; i < 11 * 23; i++) {
             map.add(0);
         }
@@ -33,11 +40,39 @@ public class TetrisActivity extends AppCompatActivity {
         gridView = (GridView) findViewById(R.id.gridView2);
         gridView.setAdapter(new TetrisGridViewAdapter(getApplicationContext(), map));
 
+        Button rotateBtn = (Button) findViewById(R.id.button1);
+        rotateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rotatePiece();
+            }
+        });
+        Button leftBtn = (Button) findViewById(R.id.button2);
+        leftBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                leftPiece();
+            }
+        });
+        Button rightBtn = (Button) findViewById(R.id.button3);
+        rightBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rightPiece();
+            }
+        });
+        Button dropBtn = (Button) findViewById(R.id.button4);
+        dropBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dropPiece();
+            }
+        });
+
         new Thread(new Runnable() {
             @Override
             public void run() {
                 while(true) {
-                    fallView();
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
@@ -45,7 +80,21 @@ public class TetrisActivity extends AppCompatActivity {
                         }
                     });
                     try {
-                        Thread.sleep(100);
+                        Thread.sleep(20);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    fallView();
+                    try {
+                        Thread.sleep(500);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -77,6 +126,86 @@ public class TetrisActivity extends AppCompatActivity {
             falling.clear();
             randomCreate();
         }
+    }
+
+    public void leftPiece() {
+        Collections.sort(falling, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return ((o1 % 11) - (o2 % 11));
+            }
+        });
+        boolean left = true;
+        for (Integer index : falling) {
+            if ((index % 11 == 0) || (stuck.contains(index - 1))) {
+                left = false;
+            }
+        }
+
+        if (left) {
+            for (int i = 0; i < 4; i ++) {
+                int index = falling.get(i);
+                int current = map.get(index);
+                map.set(index - 1, current);
+                map.set(index, 0);
+                falling.set(i, index - 1);
+            }
+        }
+    }
+
+    public void rightPiece() {
+        Collections.sort(falling, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return ((o2 % 11) - (o1 % 11));
+            }
+        });
+        boolean right = true;
+        for (Integer index : falling) {
+            if ((index % 11 == 10) || (stuck.contains(index + 1))) {
+                right = false;
+            }
+        }
+
+        if (right) {
+            for (int i = 0; i < 4; i ++) {
+                int index = falling.get(i);
+                int current = map.get(index);
+                map.set(index + 1, current);
+                map.set(index, 0);
+                falling.set(i, index + 1);
+            }
+        }
+    }
+
+    public void dropPiece() {
+        Collections.sort(falling, Collections.reverseOrder());
+        boolean drop = true;
+        for (Integer index : falling) {
+            if (stuck.contains(index + 11)) {
+                drop = false;
+            }
+        }
+
+        while(drop) {
+            for (int i = 0; i < 4; i ++) {
+                int index = falling.get(i);
+                int current = map.get(index);
+                map.set(index + 11, current);
+                map.set(index, 0);
+                falling.set(i, index + 11);
+            }
+
+            for (Integer index : falling) {
+                if (stuck.contains(index + 11)) {
+                    drop = false;
+                }
+            }
+        }
+    }
+
+    public void rotatePiece() {
+
     }
 
     public void createI() {
